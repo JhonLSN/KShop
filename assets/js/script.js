130bf5ba7f861b2cda639ca6219e60c9cf94e165
JhonLSN <jhonatanluansantos@gmail.com>
2022-01-05 17:42:10 -0300

    main

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+fetch('../assets/json/items.json')
+  .then(response => response.json())
+  .then(data => {
+
+    data.categorias.map((item) =>{
+
+    })
+  })
+
+  function dropDownListFilter(name, id) {
+    
+  }
