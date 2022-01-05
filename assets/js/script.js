9a4c6fe1035eac6822416e59f398152b24ae9ca8
JhonLSN <jhonatanluansantos@gmail.com>
2022-01-05 17:57:04 -0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
+const mainFilter = document.querySelector('.main-filter')
+let select = document.createElement('select')
+
+
 fetch('../assets/json/items.json')
-  .then(response => response.json())
-  .then(data => {
-
-    data.categorias.map((item) =>{
-
-    })
+.then(response => response.json())
+.then(data => {
+  
+  data.categorias.map((item) =>{
+    dropDownListFilter(item.nome)
   })
+})
 
-  function dropDownListFilter(name, id) {
+function dropDownListFilter(name) {
+    let option = document.createElement('option')
+    
+
+    option.text = name;
+
+    mainFilter.appendChild(select)
+    select.appendChild(option)
     
   }
