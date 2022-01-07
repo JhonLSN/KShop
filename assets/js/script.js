2d30a3144bf9ff5adbc6ec351e40901f5a8404fe
JhonLSN <jhonatanluansantos@gmail.com>
2022-01-06 22:20:50 -0300

    script items

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ fetch('../assets/json/items.json')
     })
 
     data.produtos.map((item) => {
-      showItems(item.id, item.foto, item.nome, item.descricao, item.preco)
+      showItems(item.id, item.foto, item.nome, item.descricao, item.preco, item.categoria_id)
     })
   })
 
@@ -23,7 +23,7 @@ function dropDownListFilter(name, id) {
   selectFilter.appendChild(option)
 }
 
-function showItems(id, image, name, description, price) {
+function showItems(id, image, name, description, price, category) {
   let div = document.createElement('div')
   let img = document.createElement('img')
   let h1 = document.createElement('h1')
@@ -35,7 +35,7 @@ function showItems(id, image, name, description, price) {
   paragraph.innerHTML = description;
   span.innerHTML = `${price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`;
 
-  div.setAttribute('class', 'main-items__card')
+  div.setAttribute('class', `main-items__card category-${category}`)
   div.setAttribute('id', `card-${id}`)
   div.appendChild(img)
   div.appendChild(h1)
@@ -44,15 +44,33 @@ function showItems(id, image, name, description, price) {
   sectionItems.appendChild(div)
 }
 
+
+
 selectFilter.addEventListener('click', () => {
+  const category1 = document.querySelectorAll('.category-1')
+  const category2 = document.querySelectorAll('.category-2')
+  const category3 = document.querySelectorAll('.category-3')
+
+  category1.forEach((cat1) =>
+    cat1.style.background = 'pink'
+  );
+
   if (selectFilter.value === 'Todos') {
-    sectionItems.style.backgroundColor = 'blue'
+    category1.style.display = 'flex'
+    category2.style.display = 'flex'
+    category3.style.display = 'flex'
   } else if (selectFilter.value === 'Video Games') {
-    sectionItems.style.backgroundColor = 'black'
+    category1.style.display = 'flex'
+    category2.style.display = 'none'
+    category3.style.display = 'none'
   } else if (selectFilter.value === 'Smartphones') {
-    sectionItems.style.backgroundColor = 'pink'
+    category1.style.display = 'none'
+    category2.style.display = 'flex'
+    category3.style.display = 'none'
   } else {
-    sectionItems.style.backgroundColor = 'yellow'
+    category1.style.display = 'none'
+    category2.style.display = 'none'
+    category3.style.display = 'flex'
   }
 })
 
