e8045985951e136edb891cdeb7acfd7d0032063a
JhonLSN <jhonatanluansantos@gmail.com>
2022-01-09 17:39:20 -0300

    add dark-mode

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -3,6 +3,11 @@ const header = document.querySelector('.header')
 const headerInput = document.querySelector('.header__input')
 
 btnDark.addEventListener('click', () => {
+  let btnItems = document.querySelectorAll('.main-items__card .btn')
+  
   header.classList.toggle('dark')
-  header
+  headerInput.classList.toggle('dark')
+  btnItems.forEach((btn) => {
+    btn.classList.toggle('dark')
+  })
 })
